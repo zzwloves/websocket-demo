c8446ddb46d264ba79dad5721496a442c401b04e
zzw <822819485@qq.com>
2019-05-13 18:06:34 +0800

    test commit2

--- src/main/java/io/vicp/zzwloves/websocket/GitLearning.java
+++ src/main/java/io/vicp/zzwloves/websocket/GitLearning.java
@@ -8,5 +8,7 @@ package io.vicp.zzwloves.websocket;
 public class GitLearning {
     public static void main(String[] args) {
         System.out.println("hello world");
+
+        System.out.println("hello world 2");
     }
 }
