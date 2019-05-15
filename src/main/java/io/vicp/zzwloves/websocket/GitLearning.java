9fe0ef0e914c1214666cc66aac799bd75c68dabf
zzw <822819485@qq.com>
2019-05-15 14:52:33 +0800

    'hh'

--- src/main/java/io/vicp/zzwloves/websocket/GitLearning.java
+++ src/main/java/io/vicp/zzwloves/websocket/GitLearning.java
@@ -9,5 +9,6 @@ public class GitLearning {
     public static void main(String[] args) {
         System.out.println("hello world");
         System.err.println("ddddd");
+        System.out.println("fffffff");
     }
 }
