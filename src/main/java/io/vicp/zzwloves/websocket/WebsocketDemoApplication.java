ecdb8c811b1e375a7190390ef9a89f5271d6c9f0
zzw <822819485@qq.com>
2019-08-08 13:46:26 +0800

    打war包

--- src/main/java/io/vicp/zzwloves/websocket/WebsocketDemoApplication.java
+++ src/main/java/io/vicp/zzwloves/websocket/WebsocketDemoApplication.java
@@ -2,12 +2,18 @@ package io.vicp.zzwloves.websocket;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class WebsocketDemoApplication {
+public class WebsocketDemoApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(WebsocketDemoApplication.class, args);
     }
 
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(WebsocketDemoApplication.class);
+    }
 }
